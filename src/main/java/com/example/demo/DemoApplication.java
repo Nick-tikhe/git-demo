5f1dde7b24f49e46b5c3c6bac6e11b95eed01c07
Nick-tikhe <nikhiltikhe90@gmail.com>
2024-03-14 13:46:55 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class DemoApplication {
+	
+	Logger logger=LoggerFactory.getLogger(DemoApplication.class);
+	@PostConstruct
+	public void init() {
+		logger.info("Application started....");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
