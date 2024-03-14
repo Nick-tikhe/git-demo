af90e8c8d5b45ff0d9fb0e8c4dbf8193f5fc3904
Nick-tikhe <nikhiltikhe90@gmail.com>
2024-03-14 13:52:26 +0530

    added if condition to check the data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,8 +12,11 @@ public class DemoApplication {
 	
 	Logger logger=LoggerFactory.getLogger(DemoApplication.class);
 	@PostConstruct
-	public void init() {
+	public void init(String data) {
 		logger.info("Application started....");
+		if(data.equals("java techie")) {
+			System.out.println("Welcome");
+		}
 	}
 
 	public static void main(String[] args) {
